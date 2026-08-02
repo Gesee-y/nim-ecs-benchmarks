49aa64a41944b4f759d96ed5c84fda74811bc23c
Nycto <James@RoundEights.com>
2026-08-02 07:48:57 -0700

    Remove library copies

--- libs/Necsus/tests/privateSystem.nim
+++
@@ -1,7 +0,0 @@
-import necsus, unittest, sequtils
-
-proc creator(spawn: Spawn[(string,)]) =
-  spawn.with("foo")
-
-proc assertion*(query: Query[(string,)]) {.depends(creator).} =
-  check(query.toSeq == @[("foo",)])
--- libs/polymorph/examples/ecsImport/subdir/module_1.nim
+++
@@ -1,2 +0,0 @@
-## This module exports a variable to demonstrate relative imports.
-var foo*: int
