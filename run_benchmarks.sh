ad3f44921c8f2714e234a1607ef2ef1a5c76e354
Talom Lael <gesee37@gmail.com>
2026-08-05 09:09:49 +0100

    Updated bench runner

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 cd "$(dirname "$0")"
 
 for src in src/*_bench.nim; do
-    if ! nim c -r -d:danger -o:bench_runner "$@" "$src"; then
+    if ! nim c -r -d:danger --mm:orc --cc:clang --passC:"-flto -march=native" --passL:"-flto" -o:bench_runner "$@" "$src"; then
         echo "!!! Benchmark failed for $src" >&2
         exit 1
     fi
